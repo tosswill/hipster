af3ce920dad602b4d87baa12186f241a3631b42c
christopher tosswill <tosswill@christophers-MacBook-Pro.local>
2013-11-08 21:15:11 -0800

    added json endpoint

--- hipster.rb
+++ hipster.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
+require 'active_support/all'
 
 get '/' do
   "Hello World!"
 end
 
 get '/trends' do
-  
+  vals = Parse::Query.new("TopicTrend").tap do |q|
+    q.less_eq("high_rank", 3)
+    q.greater_eq("updatedAt",Parse::Date.new(Date.today.to_s))
+  end.get
+  vals.to_json
 end
