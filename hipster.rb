0629651c29fbbc7d0e26a2a160d9d3bc6e1531de
christopher tosswill <tosswill@christophers-MacBook-Pro.local>
2013-11-08 21:18:33 -0800

    added init

--- hipster.rb
+++ hipster.rb
@@ -2,6 +2,16 @@ require 'sinatra'
 require 'active_support/all'
 require 'parse-ruby-client'
 
+Parse.init :application_id => ENV['PARSE_APPLICATION_ID'] ,
+           :api_key        => ENV['PARSE_API_KEY'] 
+
+
+Twitter.configure do |config|
+  config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token        = ENV['OAUTH_TOKEN']     
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET'] 
+
 get '/' do
   "Hello World!"
 end
