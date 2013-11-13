14b8de3bc6594267bcedc29504c33949a2a6276c
christopher tosswill <tosswill@christophers-MacBook-Pro.local>
2013-11-12 23:12:25 -0800

    added random trend endpoint

--- hipster.rb
+++ hipster.rb
@@ -25,3 +25,23 @@ get '/trends' do
   end.get
   vals.to_json
 end
+
+get '/random' do
+  count = params["count"].nil? ? 500 : params["count"].to_i
+  start_time = params["start_time"]
+  end_time =  params["end_time"]
+  min_rank =  (params["min_rank"]|| 3)
+  
+  
+  vals = Parse::Query.new("TopicTrend").tap do |q|
+    q.less_eq("high_rank", min_rank.to_i)
+    if start_time && end_time
+      q.greater_eq("updatedAt",Parse::Date.new(start_time))
+      q.less_eq("updatedAt",Parse::Date.new(end_time))
+    else
+      q.greater_eq("updatedAt",Parse::Date.new(Date.today.to_s))
+    end
+  end.get
+  vals = vals.sample(count)
+  vals.to_json
+end
