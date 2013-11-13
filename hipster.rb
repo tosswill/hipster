e82a8530a8108a08565f119551ed7e317f5de1e7
christopher tosswill <tosswill@christophers-MacBook-Pro.local>
2013-11-12 22:27:53 -0800

    added filter for min and max date and min rank

--- hipster.rb
+++ hipster.rb
@@ -6,13 +6,22 @@ Parse.init :application_id => ENV['PARSE_APPLICATION_ID'] ,
            :api_key        => ENV['PARSE_API_KEY'] 
 
 get '/' do
-  "Hello World!"
+  "Hipster API: Before it was cool."
 end
 
 get '/trends' do
+  start_time = params["start_time"]
+  end_time =  params["end_time"]
+  min_rank =  (params["min_rank"]|| 3)
+  
   vals = Parse::Query.new("TopicTrend").tap do |q|
-    q.less_eq("high_rank", 3)
-    q.greater_eq("updatedAt",Parse::Date.new(Date.today.to_s))
+    q.less_eq("high_rank", min_rank.to_i)
+    if start_time && end_time
+      q.greater_eq("updatedAt",Parse::Date.new(start_time))
+      q.less_eq("updatedAt",Parse::Date.new(end_time))
+    else
+      q.greater_eq("updatedAt",Parse::Date.new(Date.today.to_s))
+    end
   end.get
   vals.to_json
 end
